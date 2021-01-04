52f8e983d6781d37198191e95f29cd37b234d3b1
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-01-05 00:11:48 +0100

    translate function stub

--- src/compiler.rs
+++ src/compiler.rs
@@ -6,12 +6,19 @@ const ALLOCATE_AND_START: &str = include_str!("compiler/alloc_start.asm");
 /// Ditto for the end.
 const EXIT_SYSCALL: &str = include_str!("compiler/exit.asm");
 
-
-/// Transforms abstract Brainfuck instructions into assembly.
+/// Transforms abstract Brainfuck code into assembly.
 pub fn compile(code: &[BrainfuckInstr]) -> String {
     let mut output = String::new();
     output.push_str(ALLOCATE_AND_START);
-    // Here we write the rest of the damn code.
+    for instruction in code {
+        // Generate the rest of the damn code.
+        output.push_str(translate_instruction(instruction));
+        output.push('\n');
+    }
     output.push_str(EXIT_SYSCALL);
     output
+}
+/// Transforms an individual Brainfuck instruction into an x86 one.
+fn translate_instruction(instruction: &BrainfuckInstr) -> &str {
+    unimplemented!()
 }
