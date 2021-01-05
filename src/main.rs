7e64741d98e2ac7a7bd251dc3d8a7d308a99a1e0
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-01-05 03:38:26 +0100

    compiles a hello world now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bfc_rs::{compile, Parser};
+const HELLO_WORLD: &str = include_str!("../hello_world.bf");
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+    let code = match parser.parse(HELLO_WORLD) {
+        Ok(code) => code,
+        Err(e) => {
+            eprintln!("Syntax error: {:?}", e);
+            std::process::exit(1);
+        }
+    };
+    let asm = compile(&code);
+    println!("{}", asm);
 }
