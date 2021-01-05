d4b237c322453c107aae36e4690bc5732416bfc4
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-01-05 03:38:00 +0100

    fixed while loops

--- src/compiler.rs
+++ src/compiler.rs
@@ -10,16 +10,22 @@ const EXIT_SYSCALL: &str = include_str!("compiler/exit.asm");
 pub fn compile(code: &[BrainfuckInstr]) -> String {
     let mut output = String::new();
     output.push_str(ALLOCATE_AND_START);
-    let mut bracket_counter = 0;
+    let mut total_loops = 0;
+    let mut loop_no_stack = Vec::new();
     for instruction in code {
         // Generate the rest of the damn code.
-        translate_instruction(instruction, &mut bracket_counter, &mut output);
+        translate_instruction(
+            instruction,
+            &mut total_loops,
+            &mut loop_no_stack,
+            &mut output
+        );
     }
     output.push_str(EXIT_SYSCALL);
     output
 }
 /// Transforms an individual Brainfuck instruction into an x86 one and stores it in the output buffer.
-fn translate_instruction(instruction: &BrainfuckInstr, bracket_counter: &mut u32, output: &mut String) {
+fn translate_instruction(instruction: &BrainfuckInstr, total_loops: &mut u32, loop_no_stack: &mut Vec<u32>, output: &mut String) {
     use BrainfuckInstr::*;
     output.push_str(match instruction {
         PointerDec => "dec rsi",
@@ -28,20 +34,20 @@ fn translate_instruction(instruction: &BrainfuckInstr, bracket_counter: &mut u32
         DataInc => "inc byte [rsi]",
         GetByte => "mov rdx,1\nmov rdi,0\nmov rax,0\nsyscall",
         PutByte => "mov rdx,1\nmov rdi,1\nmov rax,1\nsyscall",
-        WhileNonzero=> {
+        WhileNonzero => {
+            *total_loops += 1;
+            loop_no_stack.push(*total_loops);
             let asm = format!(
-                "cmp [rsi],0\nje end_{x}\nwhile_{x}:\n",
-                x = bracket_counter
+                "cmp byte [rsi],0\nje .end_{x}\n.while_{x}:\n",
+                x = total_loops
             );
-            *bracket_counter += 1;
             output.push_str(&asm);
             return // early return so this block doesn't have to evaluate to an expression
         },
         EndWhile => {
-            *bracket_counter -= 1;
             let asm = format!(
-                "cmp [rsi],0\njne while_{x}\nend_{x}:\n",
-                x = bracket_counter
+                "cmp byte [rsi],0\njne .while_{x}\n.end_{x}:\n",
+                x = loop_no_stack.pop().expect("There should be a loop number on the stack at this point.")
             );
             output.push_str(&asm);
             return // ditto
